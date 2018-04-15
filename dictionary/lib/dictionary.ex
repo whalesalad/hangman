a0bfbc9d189f9402c5471227cb3d01932e806c01
Michael Whalen <michael@whalesalad.com>
2018-04-15 15:35:31 -0400

    Experimenting with agents!

--- dictionary/lib/dictionary.ex
+++ dictionary/lib/dictionary.ex
@@ -1,12 +1,8 @@
 defmodule Dictionary do
-  def word_list do
-    "../assets/words.txt"
-    |> Path.expand(__DIR__)
-    |> File.read!()
-    |> String.split()
-  end
 
-  def random_word do
-    Enum.random(word_list())
-  end
+  alias Dictionary.WordList
+
+  defdelegate start(), to: WordList, as: :word_list
+  defdelegate random_word(words), to: WordList
+
 end
